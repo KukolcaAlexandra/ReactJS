ae8fe0f7e41e03c86c667b30bdc7075d506131a7
Kukolca Alexandra <Alexandra_Kukolca1@epam.com>
2019-05-21 00:08:10 +0300

    [TASK-8]: delete comments

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-// import Main from './main/main.container';
 import Footer from './footer/footer.component';
 import styles from './app.css';
-// import DescriptionHeaderContainer from './header/descriptionHeaderContainer/descriptionHeader.container';
-// import MainHeaderContainer from './header/mainHeaderContainer/mainHeader.container';
-// import SortBar from './main/sortBar/sortBar.component';
-// import NoFilmFound from './main/noFilmFound/noFilmFound.component';
 import MainPage from './pages/mainPage';
 import SearchResultsPage from './pages/searchResultsPage';
 import DescriptionPage from './pages/descriptionPage';
